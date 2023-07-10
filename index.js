708860dbbf92d56da7c89b0a75b2ad3bbd67b285
ShreyashSiddharth <shreyashsiddharth@gmail.com>
2023-07-10 20:53:39 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-add = document.getElementById('add');
-add.addEventListener("click" , ()=>{
+
+function getAndUpdate(){
     console.log('Updating List...')
     tit = document.getElementById('title').value;
     desc = document.getElementById('description').value;
@@ -13,6 +13,52 @@ add.addEventListener("click" , ()=>{
         itemJsonArray.push([tit,desc]);
         localStorage.setItem('itemsJson' , JSON.stringify(itemJsonArray))
     }
-})
+    update();
+}
 
-//Watch from 2:32:59
+
+
+function update(){
+    if(localStorage.getItem('itemsJson') == null){
+        itemJsonArray = [];
+        localStorage.setItem('itemsJson' , JSON.stringify(itemJsonArray));
+    }else{
+        itemJsonArrayStr = localStorage.getItem('itemsJson')
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+      
+    }
+   
+
+    tableBody = document.getElementById('tableBody');
+    let str ="";
+    itemJsonArray.forEach((element,index) => {
+    str +=`
+    <tr>
+    <th scope="row">${index +1}</th>
+    <td>${element[0]}</td>
+    <td>${element[1]}</td>
+    <td><button class="btn btn-sm btn-primary" onclick = "deleted(${index})">Delete</button></td>
+    </tr> 
+    `
+});
+       tableBody.innerHTML = str;
+}
+add = document.getElementById('add');
+add.addEventListener("click" , getAndUpdate);
+update(); 
+function deleted(itemIndex){
+    itemJsonArrayStr  = localStorage.getItem('itemsJson')
+    itemJsonArray = JSON.parse(itemJsonArrayStr);
+    //Del
+    itemJsonArray.splice(itemIndex,1);
+    localStorage.setItem('itemsJson',JSON.stringify(itemJsonArray));
+    update();
+}
+
+
+
+
+
+
+
+
